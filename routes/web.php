0c4f598ac12dd9d70e60b19e4c787410c208c1bf
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-18 17:35:49 -0300

    alteração no arquivo web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/produtos/novo', 'App\Http\Controllers\ProdutosController@create');
+Route::post('/produtos/novo', 'App\Http\Controllers\ProdutosController@store')->name('registrar_produto');
