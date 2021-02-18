74ef70e8f78cbf09962aa01ee57b31e2fb1687a0
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-02-18 17:35:17 -0300

    criação do arquivo controller produtos

---
+++ app/Http/Controllers/ProdutosController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProdutosController extends Controller
+{
+    public function create(){
+        return view('produtos.create');
+    }
+    public function store(Request $request){
+        dd($request->all());
+    }
+}
